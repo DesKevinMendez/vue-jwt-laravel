b467bcdacc5a4def955c594358a39e3923f37fa1
Kevin Mendez <kevin_mendez30@outlook.com>
2019-09-08 14:40:01 -0600

    Se agregans vistas, y configuracion para login, register, y logout con vuex y vue-router

--- laravel-api-jwt/app/Http/Kernel.php
+++ laravel-api-jwt/app/Http/Kernel.php
@@ -19,6 +19,9 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+
+        // Laravel cors
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -40,6 +43,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
